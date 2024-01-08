cf65593d8dec15159321b13601666728aea1eb7b
Cristian Manolache <cristianmanolache0@gmail.com>
2024-01-08 20:10:24 +0200

    create user entity and connect the app to workbench database

---
+++ src/main/java/com/love2code/banking/entity/User.java
@@ -0,0 +1,38 @@
+package com.love2code.banking.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String otherName;
+    private String gender;
+    private String address;
+    private String stateOfOrigin;
+    private String accountNumber;
+    private BigDecimal accountBalance;
+    private String email;
+    private String phoneNumber;
+    private String alternativePhoneNumber;
+    private String status;
+    @CreationTimestamp
+    private LocalDateTime createAt;
+    @UpdateTimestamp
+    private LocalDateTime modifiedAt;
+}
