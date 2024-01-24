9b9c5bb15844eae1524684bead0f0a322e518153
Cristian Manolache <cristianmanolache0@gmail.com>
2024-01-24 11:17:08 +0200

    add generate statement pdf and send it to email with attachement

--- src/main/java/com/love2code/banking/service/EmailService.java
+++ src/main/java/com/love2code/banking/service/EmailService.java
@@ -4,4 +4,5 @@ import com.love2code.banking.dto.EmailDetails;
 
 public interface EmailService {
     void sendEmailAlert(EmailDetails emailDetails);
+    void sendEmailWithAttachment(EmailDetails emailDetails);
 }
